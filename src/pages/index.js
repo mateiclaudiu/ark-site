224cc41e29e0165e773fc2716b6b44f8137b89aa
Matei <info.claudiu@gmail.com>
2020-04-01 21:26:01 +0200

    Remove Gatsby boiler html

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,11 +61,11 @@ const IndexPage = () => (
       </HelloContainerStyled>
     </div>
 
-    <p>Now go build something great.</p>
+  {/*  <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image/>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2</Link>*/}
   </Layout>
 )
 
