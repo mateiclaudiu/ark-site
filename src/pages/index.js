15798d76a3a0dcee5ce4e336ee05f348871a1012
Matei <info.claudiu@gmail.com>
2020-04-16 12:10:18 +0200

    Add hardcoded mobile navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,40 @@ import { Event, UpcomingEventList } from "../components/event"
 import introImage from "../images/together.jpg"
 import { Contact } from "../components/contact"
 
+const NavStyled = styled.div`
+  font-family: Poppins;
+  font-size: 0.9rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  color:white;
+  background:#31333b;
 
+  ul{
+    margin: 0;
+  }
+  
+  li{
+    border-top: 1px solid #707176;
+    list-style: none;
+    margin: 0;
+    padding: 0.5rem 0.5rem 0.5rem 1rem;
+  }
+  
+  a{
+    text-decoration: none;
+    color: white;
+  }
+  li:hover{
+    text-decoration: none;
+    cursor: pointer;
+    background:#2a2c33;
+    transition: 0.3s;
+  }
+  
+  @media (min-width: 768px) {
+    display:none;
+  } 
+`
 
 const IndexPage = () => {
   const { events, websites } = useStaticQuery(graphql`
@@ -40,6 +73,12 @@ const IndexPage = () => {
 
   return <Layout>
     <SEO title="Home"/>
+    <NavStyled>
+      <ul id="navigation">
+        <li><Link to="/wieiswie/">Wie is wie?</Link></li>
+        <li><Link to="/links/">Handige links</Link></li>
+      </ul>
+    </NavStyled>
     <Intro image={introImage}/>
     <Feature/>
     <PageContainer>
@@ -50,7 +89,7 @@ const IndexPage = () => {
       <UpcomingEventList events={events.edges}/>
     </PageContainer>
     <Link to="/wieiswie/">Wie is wie?</Link>
-    <Contact />
+    <Contact/>
   </Layout>
 }
 
