0b7be0716b1a64e0f570b832aa26fe790b84873e
Matei <info.claudiu@gmail.com>
2020-04-16 14:28:29 +0200

    Move navbar Header and show navbar on desktop as well

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,79 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+
+const NavStyled = styled.div`
+  display: ${props => props.display};
+  font-family: Poppins;
+  font-size: 0.9rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  color:white;
+  background:#31333b;
+
+  ul{
+    margin: 0;
+  }
+  
+  li{
+    border-top: 1px solid #707176;
+    list-style: none;
+    margin: 0;
+    padding: 0.5rem 0.5rem 0.5rem 0rem;
+  }
+  
+  a{
+    text-decoration: none;
+    color: gray;
+  }
+  li:hover{
+    text-decoration: none;
+    cursor: pointer;
+    background:#2a2c33;
+    transition: 0.3s;
+  }
+  
+  @media (min-width: 768px) {
+    //display:none;
+    ul{
+      display: flex;  
+      justify-content: center;
+    }
+    
+    li{
+      padding: 0 3rem;
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+      border-top: none;
+      border-right: 1px solid #707176;
+      
+      a{
+        color: white;
+      }
+    }
+     
+    li:hover{
+      text-decoration: none;
+      cursor: pointer;
+      background:inherit;
+    
+    a{
+      color: gray;
+      transition: 0.7s;
+    }
+  } 
+    li:last-child{
+      border-top: none;
+      border-right: none;
+    }
+  } 
+`
+
+NavStyled.defaultProps = {
+  display: "visible",
+}
 
 const Header = ({ siteTitle }) => (
   <header
@@ -27,6 +100,13 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <NavStyled>
+        <ul id="navigation">
+          <li><Link to="/index/">Home</Link></li>
+          <li><Link to="/wieiswie/">Wie is wie?</Link></li>
+          <li><Link to="/links/">Handige links</Link></li>
+        </ul>
+      </NavStyled>
     </div>
   </header>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -14,45 +14,6 @@ import "./layout.css"
 import { FooterStyled } from "./styled"
 import styled from "styled-components"
 
-const NavStyled = styled.div`
-  display: ${props => props.display};
-  font-family: Poppins;
-  font-size: 0.9rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  color:white;
-  background:#31333b;
-
-  ul{
-    margin: 0;
-  }
-  
-  li{
-    border-top: 1px solid #707176;
-    list-style: none;
-    margin: 0;
-    padding: 0.5rem 0.5rem 0.5rem 1rem;
-  }
-  
-  a{
-    text-decoration: none;
-    color: gray;
-  }
-  li:hover{
-    text-decoration: none;
-    cursor: pointer;
-    background:#2a2c33;
-    transition: 0.3s;
-  }
-  
-  @media (min-width: 768px) {
-    display:none;
-  } 
-`
-
-NavStyled.defaultProps = {
-  display: "visible",
-}
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -70,19 +31,14 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={data.site.siteMetadata.title}/>
       <div
         style={{
           margin: `0 auto`,
-         // maxWidth: 960,
-         // padding: `0 1.0875rem 1.45rem`,
+          // maxWidth: 960,
+          // padding: `0 1.0875rem 1.45rem`,
         }}
-      ><NavStyled>
-        <ul id="navigation">
-          <li><Link to="/wieiswie/">Wie is wie?</Link></li>
-          <li><Link to="/links/">Handige links</Link></li>
-        </ul>
-      </NavStyled>
+      >
         <main>{children}</main>
         <FooterStyled>
           © {new Date().getFullYear()}, Built by
