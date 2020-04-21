ddbabccadca09a010d7b1738cbe2168ec771554b
Matei <info.claudiu@gmail.com>
2020-04-21 17:37:27 +0200

    Not found page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,13 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { SectionTitle } from "../components/section-title"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Not found"/>
+    <SectionTitle title={"OEPS. PAGINA NIET GEVONDEN"} subtitle={"Hij geeft mij nieuwe kracht en leidt mij langs veilige paden tot eer van zijn naam.- Psalm" +
+    " 23:3"}/>
   </Layout>
 )
 
