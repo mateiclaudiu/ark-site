9b1a69512dd9719801fbdab660b6f2bb4771b3d0
Claudiu Matei <info.claudiu@gmail.com>
2021-12-13 14:16:17 +0100

    Oikoumene styling

--- src/pages/oikumene.js
+++ src/pages/oikumene.js
@@ -2,15 +2,18 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { SectionTitle } from "../components/section-title"
+import {SectionTitle} from "../components/section-title"
 import pdfDocument from "../../static/0_KAFT_OIKOUMENE_2021-4_proef_1.pdf"
+import {PageContainer} from "../components/page-container";
 
 const Oikumene = () => (
-  <Layout>
-    <SEO title="Oikumene"/>
-    <SectionTitle title={"Oikumene"} subtitle={""}/>
-      <a href={pdfDocument} download>Klik hier om de Oikumene van oktober-november-december te downloaden</a>
-  </Layout>
+    <Layout>
+        <SEO title="Oikumene"/>
+        <SectionTitle title={"Oikumene"} subtitle={""}/>
+        <PageContainer>
+            <a href={pdfDocument} download>Klik <b>hier</b> om de Oikumene van oktober-november-december 2021 te downloaden</a>
+        </PageContainer>
+    </Layout>
 )
 
 export default Oikumene
