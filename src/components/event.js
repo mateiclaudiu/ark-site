607d2fbb4bb83c93e6119be6528e24b8ecf0891b
claudiu <info.claudiu@gmail.com>
2022-10-06 20:52:22 +0200

    add new events

--- src/components/event.js
+++ src/components/event.js
@@ -26,9 +26,10 @@ export const Event = ({dayNumber, monthName, dayName, time = "11:00", title, inf
 
 export const UpcomingEventList = ({events}) => {
     const activeEvents = events.filter(({node}) => new Date(node.eventDate) > new Date());
-    let titleStyled =  <ItalicTitleStyled fontSize={"1rem"} color={"gray"}>{
+    let titleStyled = <ItalicTitleStyled fontSize={"1rem"} color={"gray"}>{
         <>
-            Een overzicht van de deelnemende kerken kunt u vinden in onze <a href={pdfDocument}><b>brochure (klik hier)</b></a> of op de website <a href='https://www.topa.be/'><b>www.topa.be</b></a>
+            Een overzicht van de deelnemende kerken kunt u vinden in onze <a href={pdfDocument}><b>brochure (klik
+            hier)</b></a> of op de website <a href='https://www.topa.be/'><b>www.topa.be</b></a>
         </>
     }</ItalicTitleStyled>;
 
@@ -62,10 +63,10 @@ export const UpcomingEventList = ({events}) => {
                     key={node.title + node.dayNumber}
                 />
             ))}
-            {/*<div>Na 29 juni gaan wij er even tussenuit. Wij wensen iedereen een fijne*/}
-            {/*    zomer toe.*/}
-            {/*    Wij heten jullie opnieuw welkom op 31 augustus in de Prinsstraat 13a.*/}
-            {/*    DANK aan al onze voorgangers.</div>*/}
+            <div>We houden een onderbreking
+                van woensdag 21 dec 2022 tot en met woensdag 4 januari 2023
+                en hervatten op woensdag 11 januari 2023
+            </div>
             {
                 activeEvents.length === 0 ? (<div>Er zijn geen evenementen gepland.</div>) : <></>
             }
--- src/pages/oikumene.js
+++ src/pages/oikumene.js
@@ -1,19 +1,19 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import {SectionTitle} from "../components/section-title"
-import pdfDocument from "../../static/0_KAFT_OIKOUMENE_2021-4_proef_1.pdf"
-import {PageContainer} from "../components/page-container";
-
-const Oikumene = () => (
-    <Layout>
-        <SEO title="Oikumene"/>
-        <SectionTitle title={"Oikumene"} subtitle={""}/>
-        <PageContainer>
-            <a href={pdfDocument} download>Klik <b>hier</b> om de Oikumene van oktober-november-december 2021 te downloaden</a>
-        </PageContainer>
-    </Layout>
-)
-
-export default Oikumene
+// import React from "react"
+//
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import {SectionTitle} from "../components/section-title"
+// import pdfDocument from "../../static/0_KAFT_OIKOUMENE_2021-4_proef_1.pdf"
+// import {PageContainer} from "../components/page-container";
+//
+// const Oikumene = () => (
+//     <Layout>
+//         <SEO title="Oikumene"/>
+//         <SectionTitle title={"Oikumene"} subtitle={""}/>
+//         <PageContainer>
+//             <a href={pdfDocument} download>Klik <b>hier</b> om de Oikumene van oktober-november-december 2021 te downloaden</a>
+//         </PageContainer>
+//     </Layout>
+// )
+//
+// export default Oikumene
