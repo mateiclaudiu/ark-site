55c39ef9a7b979933c182637a3858f7994791b71
claudiu <info.claudiu@gmail.com>
2023-08-23 20:32:22 +0200

    special events

--- src/pages/bijzondere-events.js
+++ src/pages/bijzondere-events.js
@@ -4,13 +4,16 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {SectionTitle} from "../components/section-title"
 import {SpecialEvent} from "../components/special-events";
+import {PageContainer} from "../components/page-container";
 
 const BijzondereEvents = () => (
   <Layout>
     <SEO title="Bijzondere Events"/>
 
       <SectionTitle title={"Bijzondere Events"} subtitle={""}/>
-      <SpecialEvent></SpecialEvent>
+      <PageContainer>
+          <SpecialEvent></SpecialEvent>
+      </PageContainer>
   </Layout>
 )
 
