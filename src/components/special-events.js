75430608dea2eeededada99e074095f4faa782fe
claudiu <info.claudiu@gmail.com>
2023-08-23 20:29:57 +0200

    special events

--- src/components/special-events.js
+++ src/components/special-events.js
@@ -3,53 +3,50 @@ import {color3} from "./colors";
 import React from "react";
 import fototentoonstelling from "../images/2023 ark tentoonstelling affiche.jpg"
 
-export const SpecialEvent = () => (
-    <>
-        <div style={{textAlign: "center", background: "#bdd7ee"}}>
-            <img src={fototentoonstelling}/>
-        </div>
+export const SpecialEvent = () => (<>
+    <div style={{textAlign: "center", background: "#bdd7ee"}}>
+        <img src={fototentoonstelling}/>
+    </div>
 
-        <EventStyled>
-            <EventDateStyled>
-                <EventDayStyled>
-                    <TitleStyled fontSize={"2rem"} color={"lightgray"}>{"10 t.e.m. 24"}</TitleStyled>
-                </EventDayStyled>
-                <div>
-                    <TitleStyled fontSize={"1rem"} color={"gray"}>{"September"}</TitleStyled>
-                    {/*<TitleStyled fontSize={"1rem"} color={"black"}>{dayName}</TitleStyled>*/}
-                    <TitleStyled fontSize={"1rem"}
-                                 color={"black"}>{"voor en na de vieringen op zondag en woensdag"}</TitleStyled>
-                </div>
-            </EventDateStyled>
+    <EventStyled>
+        <EventDateStyled>
+            <EventDayStyled>
+                <TitleStyled fontSize={"2rem"} color={"lightgray"}>{"10 t.e.m. 24"}</TitleStyled>
+            </EventDayStyled>
             <div>
-                <TitleStyled fontSize={"1.6rem"}
-                             color={color3}>{"Fototentoonstelling, een unieke kijk in de diversiteit van de kerk van vandaag"}</TitleStyled><br/>
-                {/*<ItalicTitleStyled fontSize={"1rem"} color={"gray"}>Info: {info}</ItalicTitleStyled><br/>*/}
-                <ItalicTitleStyled fontSize={"1rem"}
-                                   color={"gray"}>Plaats: {"Sint-Niklaaskerk Dessel"}</ItalicTitleStyled>
-                <ItalicTitleStyles>
-                    In het kader van het 50-jarig jubileum van de Antwerpse Raad van Kerken werd in oktober en
-                    november 2022 een grote fototentoonstelling georganiseerd. Deze vond plaats op diverse locaties in
-                    het centrum van Antwerpen. We mochten hiervoor meer dan 1000 bezoekers verwelkomen.
-                    Uit honderden foto’s werden er 130 geselecteerd. Die gaven een overzicht van de gebruiken en
-                    rituelen in de 11 lidkerken de ARK. <br/>
-                    Het zou zonde zijn om dit prachtig materiaal nu in een kast in de kelder te steken. Daarom hebben
-                    we besloten om delen van deze collectie op diverse plaatsen tentoon te stellen. Nadat we reeds
-                    gebruik mochten maken van de gastvrijheid van het Sint-Anneke Centrum op Linkeroever, geven we
-                    hieronder een overzicht van de volgende geplande exposities:
-                    Dessel:<br/>
-                    Sint – Niklaaskerk Turnhoutsebaan Dessel-centrum<br/>
-                    Zondagen 10, 17 en 24 september 2023 voor en na de misviering (9.30 uur).<br/>
-                    <br/>
-                    Merksem:<br/>
-                    O. L. V. van Smarten Pastoor Bampsstraat 1, Merksem<br/>
-                    Zondagen 8, 15 en 22 oktober 2023 voor en na de misviering (10.30 uur).<br/>
-                    <br/>
-                    Wijnegem:<br/>
-                    O. L. V. van de Bloeiende Wijngaardkerk Wommelgemsesteenweg 34 Wijnegem<br/>
-                    Januari 2024 (details volgen).
-                </ItalicTitleStyles>
+                <TitleStyled fontSize={"1rem"} color={"gray"}>{"September"}</TitleStyled>
+                {/*<TitleStyled fontSize={"1rem"} color={"black"}>{dayName}</TitleStyled>*/}
+                <TitleStyled fontSize={"1rem"}
+                             color={"black"}>{"voor en na de vieringen op zondag en woensdag"}</TitleStyled>
             </div>
-        </EventStyled>
-    </>
-)
+        </EventDateStyled>
+        <div>
+            <TitleStyled fontSize={"1.6rem"}
+                         color={color3}>{"Fototentoonstelling, een unieke kijk in de diversiteit van de kerk van vandaag"}</TitleStyled><br/>
+            {/*<ItalicTitleStyled fontSize={"1rem"} color={"gray"}>Info: {info}</ItalicTitleStyled><br/>*/}
+            <ItalicTitleStyled fontSize={"1rem"} color={"gray"}>Plaats: {"Sint-Niklaaskerk Dessel"}</ItalicTitleStyled>
+            <ItalicTitleStyled>
+                In het kader van het 50-jarig jubileum van de Antwerpse Raad van Kerken werd in oktober en
+                november 2022 een grote fototentoonstelling georganiseerd. Deze vond plaats op diverse locaties in
+                het centrum van Antwerpen. We mochten hiervoor meer dan 1000 bezoekers verwelkomen.
+                Uit honderden foto’s werden er 130 geselecteerd. Die gaven een overzicht van de gebruiken en
+                rituelen in de 11 lidkerken de ARK. <br/>
+                Het zou zonde zijn om dit prachtig materiaal nu in een kast in de kelder te steken. Daarom hebben
+                we besloten om delen van deze collectie op diverse plaatsen tentoon te stellen. Nadat we reeds
+                gebruik mochten maken van de gastvrijheid van het Sint-Anneke Centrum op Linkeroever, geven we
+                hieronder een overzicht van de volgende geplande exposities:
+                Dessel:<br/>
+                Sint – Niklaaskerk Turnhoutsebaan Dessel-centrum<br/>
+                Zondagen 10, 17 en 24 september 2023 voor en na de misviering (9.30 uur).<br/>
+                <br/>
+                Merksem:<br/>
+                O. L. V. van Smarten Pastoor Bampsstraat 1, Merksem<br/>
+                Zondagen 8, 15 en 22 oktober 2023 voor en na de misviering (10.30 uur).<br/>
+                <br/>
+                Wijnegem:<br/>
+                O. L. V. van de Bloeiende Wijngaardkerk Wommelgemsesteenweg 34 Wijnegem<br/>
+                Januari 2024 (details volgen).
+            </ItalicTitleStyled>
+        </div>
+    </EventStyled>
+</>);
