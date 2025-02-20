570469406a9b3fc39540bfc9b60d82a809cbac54
claudiu <info.claudiu@gmail.com>
2025-02-20 14:56:08 +0100

    Dag diakonie

--- src/pages/bijzondere-events.js
+++ src/pages/bijzondere-events.js
@@ -5,15 +5,17 @@ import SEO from "../components/seo"
 import {SectionTitle} from "../components/section-title"
 import {SpecialEvent} from "../components/special-events";
 import {PageContainer} from "../components/page-container";
+import DagDiaconieFolder from "../images/Folder ARK DagDiaconie 210x297mm 29-03-25.jpg";
 
 const BijzondereEvents = () => (
-  <Layout>
-    <SEO title="Bijzondere Events"/>
+    <Layout>
+        <SEO title="Bijzondere Events"/>
 
-      <SectionTitle title={"Bijzondere Events"} subtitle={""}/>
-      Voorlopig zijn er geen bijzondere events gepland.
-      {/*<SpecialEvent></SpecialEvent>*/}
-  </Layout>
+        <SectionTitle title={"Bijzondere Events"} subtitle={""}/>
+        {/*<SpecialEvent></SpecialEvent>*/}
+        <img src={DagDiaconieFolder}/>
+
+    </Layout>
 )
 
 export default BijzondereEvents
